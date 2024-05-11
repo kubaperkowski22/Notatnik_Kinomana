3100b919ef3afc1af6262a47c2aff992c992ad8d
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-05-11 20:05:38 +0200

    1st commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Notatnik_Kinomana_v2.ViewModels;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notatnik_Kinomana_v2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AdonisWindow : Window
+    {
+        public MainWindowVM ViewModel
+        {
+            get
+            {
+                return _viewModel;
+            }
+            set
+            {
+                _viewModel = value;
+            }
+        }
+        private MainWindowVM _viewModel;
+        public AdonisWindow()
+        {
+            ViewModel = new MainWindowVM();
+
+            this.DataContext = ViewModel;
+        }
+    }
+}
---
+++ ViewModels/MainWindowVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Notatnik_Kinomana_v2.ViewModels
+{
+    public class MainWindowVM
+    {
+
+        public MainWindowVM()
+        {
+
+        }
+
+        public void ShowMessage()
+        {
+            MessageBox.Show("to jest wiadomosc","hejka", MessageBoxButton.OK,MessageBoxImage.Information);
+        }
+    }
+}
