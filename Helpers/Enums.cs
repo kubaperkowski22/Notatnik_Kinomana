dcfde951193c2297bd5541bf43b961f52a54a07d
Jakub Perkowski <kubaperkowski22@gmail.com>
2024-05-31 00:17:00 +0200

    'Familijny' category added

--- Helpers/Enums.cs
+++ Helpers/Enums.cs
@@ -19,6 +19,8 @@ namespace Notatnik_Kinomana_v2.Helpers
         Dokument,
         [Description("Film dramatyczny")]
         Dramat,
+        [Description("Film familijny")]
+        Familijny,
         [Description("Film fantasy")]
         Fantasy,
         [Description("horror")]
